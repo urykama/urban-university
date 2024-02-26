8b5c16ad35bc09d05d9d21bb8515c11b4591c306
URAL KAMALETDINOV <urykama@gmail.com>
2024-02-26 20:28:58 +0500

    добавление 2023/12/31 00:00|Домашнее задание по теме "Интроспекция"
    https://urban-university.ru/members/courses/course999421818026/20231231-0000domasnee-zadanie-po-teme-introspekcia-748607565945

---
+++ 20231231_Интроспекция.py
@@ -0,0 +1,24 @@
+import inspect
+import pprint
+
+
+def introspection_info(
+        obj: object = lambda: True):  # 1. Создайте функцию introspection_info(obj), которая принимает объект obj.
+    return {
+        'Имя обекта:                    ': obj.__name__,
+        'Тип объекта      (type)        ': type(obj),  # - Тип объекта.
+        'Атрибуты объекта (attributes)  ': obj.__dict__,  # - Атрибуты объекта.
+        'Методы объекта   (methods)     ': dir(obj),  # - Методы объекта.
+        'Модуль, к которому объект принадлежит': obj.__module__,  # - Модуль, к которому объект принадлежит
+        'Модуль, к которому объект принадлежит 2': inspect.getmodule(obj),  # - Модуль, к которому объект принадлежит
+        'signature                      ': inspect.signature(obj),  # - Модуль, к которому объект принадлежит
+        'isinstance(obj)                ': isinstance(obj, (float, int, str, list, dict, tuple,)),
+        'inspect.isfunction(obj)        ': inspect.isfunction(obj),
+
+    }
+
+
+if __name__ == '__main__':
+    pprint.pprint(introspection_info(introspection_info))
+    print()
+    pprint.pprint(introspection_info())
