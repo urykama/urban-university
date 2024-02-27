d9bb27c33d41577696ab2f196988d8af0e41ac4c
URAL KAMALETDINOV <urykama@gmail.com>
2024-02-27 10:50:20 +0500

    добавление 2024/01/12 00:00|Домашнее задание по теме "Тестирование"
    https://urban-university.ru/members/courses/course999421818026/20240112-0000domasnee-zadanie-po-teme-testirovanie-650162469576

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-# This is a sample Python script.
+class Student:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
 
+    def run(self):
+        self.distance += 10
 
-def print_hi(name):
+    def walk(self):
+        self.distance += 5
 
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __str__(self):
+        return self.name
